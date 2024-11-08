b32848b10a2cffcee3fe3cf0230d244faa1f19c5
Aina Oluwatimilehin <wizkevinaaina@gmail.com>
2024-11-08 17:14:38 +0100

    datasets to github

---
+++ EDA.py
@@ -0,0 +1,15 @@
+# Distribution of 'Purchase' variable
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(10, 6))
+sns.histplot(df['Purchase'], bins=30, kde=True)
+plt.title('Distribution of Purchase Amounts')
+plt.xlabel('Purchase Amount')
+plt.ylabel('Frequency')
+plt.show()
+
+# Explore average purchase amount by age group
+plt.figure(figsize=(12, 6))
+sns.barplot(x='Age', y='Purchase', data=df, estimator=np.mean)
+plt.title('Average Purchase by Age Group')
+plt.show()
